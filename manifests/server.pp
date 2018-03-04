e434181d39fca9f2cf7c692f2674dd3237c1896b
scott.liao <shazi7804@gmail.com>
2018-03-05 01:18:00 +0800

    add smb global config

--- manifests/server.pp
+++ manifests/server.pp
@@ -6,12 +6,45 @@ class corp104_samba::server (
   String $service_name,
   String $smb_config,
   Boolean $nmbd_enable,
+  String $workgroup,
+  String $server_string,
+  Boolean $dns_proxy,
+  Boolean $wins_support,
+  Optional[String] $wins_server,
+  Optional[String] $interface,
+  Boolean $bind_interfaces_only,
+  String $log_file,
+  Integer $max_log_size,
+  Boolean $syslog_only,
+  String $syslog,
+  String $panic_action,
+  String $server_role,
+  String $passdb_backend,
+  Boolean $obey_pam_restrictions,
+  Boolean $unix_password_sync,
+  String $passwd_program,
+  String $passwd_chat,
+  Boolean $pam_password_change,
+  String $map_to_guest,
+  Optional[String] $logon_path,
+  Optional[String] $logon_drive,
+  Optional[String] $logon_home,
+  Optional[String] $logon_script,
+  Optional[String] $add_user_script,
+  Optional[String] $add_machine_script,
+  Optional[String] $add_group_script,
+  Optional[String] $include,
+  Optional[String] $idmap_uid,
+  Optional[String] $idmap_gid,
+  Optional[String] $template_shell,
+  Optional[Integer] $usershare_max_shares,
+  Boolean $usershare_allow_guests,
 ){
-  contain corp104_samba::install
-  contain corp104_samba::config
-  contain corp104_samba::service
+  contain corp104_samba::server::install
+  contain corp104_samba::server::config
+  contain corp104_samba::server::service
 
-  Class['::corp104_samba::install']
-  -> Class['::corp104_samba::config']
-  ~> Class['::corp104_samba::service']
+  Class['::corp104_samba::server::install']
+  -> Class['::corp104_samba::server::config']
+  ~> Class['::corp104_samba::server::service']
 }
--- manifests/server/config.pp
+++ manifests/server/config.pp
@@ -1,7 +1,98 @@
 class corp104_samba::server::config inherits corp104_samba::server {
-  augeas { 'smb.conf':
-    incl => $smb_config,
-    lens => 'Samba.lns',
-    notify  => Service['corp104_samba']
+  file { '/etc/samba':
+    ensure => directory,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+  }
+
+  concat { $corp104_samba::server::smb_config:
+    ensure => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    require => [ File['/etc/samba'], Package['samba'] ],
+    notify  => Class['corp104_samba::server::service'],
+  }
+
+  # Template uses:
+  # - $workgroup
+  # - $server_string
+  concat::fragment { 'smb.conf-global':
+    target  => $corp104_samba::server::smb_config,
+    order   => 0,
+    content => template("${module_name}/config/smb_conf/global.erb"),
+  }
+
+  # Template uses:
+  # - $wins_support
+  # - $wins_server
+  concat::fragment { 'smb.conf-global-wins':
+    target  => $corp104_samba::server::smb_config,
+    order   => 10,
+    content => template("${module_name}/config/smb_conf/global_wins.erb"),
+  }
+
+  # Template uses:
+  # - $interfaces
+  # - $bind_interfaces_only
+  concat::fragment { 'smb.conf-global-networking':
+    target  => $corp104_samba::server::smb_config,
+    order   => 12,
+    content => template("${module_name}/config/smb_conf/global_networking.erb"),
+  }
+
+  # Template uses:
+  # - $log_file
+  # - $max_log_size
+  # - $syslog_only
+  # - $syslog
+  # - $panic_action
+  concat::fragment { 'smb.conf-global-debugging':
+    target  => $corp104_samba::server::smb_config,
+    order   => 13,
+    content => template("${module_name}/config/smb_conf/global_debugging.erb"),
+  }
+
+  # Template uses:
+  # - $server_role
+  # - $passdb_backend
+  # - $obey_pam_restrictions
+  # - $unix_password_sync
+  # - $passwd_program
+  # - $passwd_chat
+  # - $pam_password_change
+  # - $map_to_guest
+  concat::fragment { 'smb.conf-global-authentication':
+    target  => $corp104_samba::server::smb_config,
+    order   => 14,
+    content => template("${module_name}/config/smb_conf/global_authentication.erb"),
+  }
+
+  # Template uses:
+  # - $logon_path
+  # - $logon_drive
+  # - $logon_home
+  # - $logon_script
+  # - $add_user_script
+  # - $add_machine_script
+  # - $add_group_script
+  concat::fragment { 'smb.conf-global-domain':
+    target  => $corp104_samba::server::smb_config,
+    order   => 15,
+    content => template("${module_name}/config/smb_conf/global_domain.erb"),
+  }
+
+  # Template uses:
+  # - $include
+  # - $idmap_uid
+  # - $idmap_gid
+  # - $template_shell
+  # - $usershare_max_shares
+  # - $usershare_allow_guests
+  concat::fragment { 'smb.conf-global-misc':
+    target  => $corp104_samba::server::smb_config,
+    order   => 16,
+    content => template("${module_name}/config/smb_conf/global_misc.erb"),
   }
 }
